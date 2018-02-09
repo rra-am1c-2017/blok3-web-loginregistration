a59cf1e7935bc89b1187bba1148581685b78a93d
Arjan de Ruijter <rra.am1a.2017@gmail.com>
2018-02-09 11:20:42 +0100

    nocheck

--- register.php
+++ register.php
@@ -1,44 +1,42 @@
 <?php
-  var_dump($_POST); exit();
-  //   Maak contact met de database en server
+  // var_dump($_POST); exit();
+  // Maak contact met de database en server
 
   include("./db_connect.php");
   include("./functions/sanitize.php");
 
+  if (isset($_POST["conditions"])) {
+    // We maken de vieze $_POST waarden schoon
+    $email = sanitize($_POST["email"]);
+    $conditions = sanitize($_POST["conditions"]);
 
+    // Hier checken we of het emailadres bestaat in de database. We maken de select query
+    $sql = "SELECT * FROM `login` WHERE `email` = '{$email}'";
 
-  // We maken de vieze $_POST waarden schoon
-  $email = sanitize($_POST["email"]);
-  $conditions = sanitize($_POST["conditions"]);
-
-  // Hier checken we of het emailadres bestaat in de database. We maken de select query
-  $sql = "SELECT * FROM `login` WHERE `email` = '{$email}'";
-
-  // We vuren de query af op de database
-  $result = mysqli_query($conn, $sql);
-
-  // Test of het aantal geselecteerde rijen 0 (false) of 1(true)
-  if (mysqli_num_rows($result)) {
-    // Het emailadres bestaat al in database
-    header("Location: ./index.php?status=emailexists");
-  } else {    
-    // We maken onze query voor het inserten van de formuliergegevens.
-    $sql = "INSERT INTO `login` (`id`,
-                                 `email`,
-                                 `conditions`,
-                                 `userrole`)
-                VALUES          (NULL,
-                                '{$email}',
-                                '{$conditions}',
-                                'subscriber')";
-    //echo $sql; exit();  
     // We vuren de query af op de database
-    mysqli_query($conn, $sql);
-    // Stuur de gebruiker door naar index.php
-    header("Location: ./index.php?status=succes");
-  }
+    $result = mysqli_query($conn, $sql);
 
-  
-  
-   
+    // Test of het aantal geselecteerde rijen 0 (false) of 1(true)
+    if (mysqli_num_rows($result)) {
+      // Het emailadres bestaat al in database
+      header("Location: ./index.php?status=emailexists");
+    } else {    
+      // We maken onze query voor het inserten van de formuliergegevens.
+      $sql = "INSERT INTO `login` (`id`,
+                                  `email`,
+                                  `conditions`,
+                                  `userrole`)
+                  VALUES          (NULL,
+                                  '{$email}',
+                                  '{$conditions}',
+                                  'subscriber')";
+      //echo $sql; exit();  
+      // We vuren de query af op de database
+      mysqli_query($conn, $sql);
+      // Stuur de gebruiker door naar index.php
+      header("Location: ./index.php?status=succes");
+    }
+  } else {
+    header("Location: ./index.php?status=nocheck");
+}   
 ?>
