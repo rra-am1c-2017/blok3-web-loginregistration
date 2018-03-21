c3705498bb62f16e3e8900903ee4e3895bafd3a3
Arjan de Ruijter <rra.am1a.2017@gmail.com>
2018-03-21 11:23:48 +0100

    change userrole

--- navigation.php
+++ navigation.php
@@ -1,16 +1,17 @@
 <?php  
   if (isset($_SESSION["id"])) {
-
+    echo '<a href="./index.php?action=home">Home</a> ';
     switch($_SESSION["userrole"]) {
       case "subscriber":
-        echo '<a href="./index.php?action=subscriber_home">Home</a> ';
+        echo '<a href="./index.php?action=subscriber_home">subscriber Home</a> ';
         break;
       case "administrator":
-        echo '<a href="./index.php?action=administrator_home">Home</a> ';
+        echo '<a href="./index.php?action=administrator_home">admin Home</a> ';
         break;
       case "superadmin":
-        echo '<a href="./index.php?action=superadmin_home">Home</a> ';
-        echo '<a href="./index.php?action=administrator_home">Administrator Home</a> ';        
+        echo '<a href="./index.php?action=superadmin_home">superadmin Home</a> ';
+        echo '<a href="./index.php?action=administrator_home">Administrator Home</a> '; 
+        echo '<a href="./index.php?action=change_userrole">Verander gebruikersrol</a> '; 
         break;
       default:
         header("Location: ./index.php?action=home");
@@ -20,7 +21,7 @@
     echo '<a href="./index.php?action=games">Games</a> ';    
     echo '<a href="./logout.php">Uitloggen</a> ';
   } else {
-    echo '<a href="./index.php?action=home">Home</a> ';
+    
     echo '<a href="./index.php?action=registerform&amp;status=register">Registreer</a> ';
     echo '<a href="./index.php?action=loginform&amp;status=login">Inloggen</a> ';
   }
